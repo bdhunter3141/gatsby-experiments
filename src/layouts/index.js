07f280a7f927cc65e86ab9540193d511b6ede42e
Bryan Hunter <bdhunter3141@gmail.com>
2018-04-21 20:10:25 -0400

    finished setting up cookie for token

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-const axios = require('axios');
+const axios = require('axios')
 import Header from '../components/header'
 import './index.css'
 import Cookies from 'js-cookie'
@@ -19,48 +19,58 @@ export const query = graphql`
 
 export default class Layout extends Component {
   static propTypes = {
-  children: PropTypes.func,
+    children: PropTypes.func,
   }
   state = {
-    token: ''
+    token: null,
   }
   componentDidMount() {
-    this.getToken();
+    if (!Cookies.get('token')) {
+      console.log('getting token')
+      this.getToken()
+    }
   }
   getToken = () => {
-    axios.post(`${process.env.API_AUTH_URL}`, `grant_type=client_credentials&client_id=${process.env.API_CLIENT_ID}&client_secret=${process.env.API_CLIENT_SECRET}`)
-      .then((res) => {
-        this.setState({ token: res.data.access_token})
-        Cookies.set('token', res.data.access_token)
+    axios
+      .post(
+        `${process.env.API_AUTH_URL}`,
+        `grant_type=client_credentials&client_id=${
+          process.env.API_CLIENT_ID
+        }&client_secret=${process.env.API_CLIENT_SECRET}`
+      )
+      .then(res => {
+        this.setState({ token: true })
+        Cookies.set('token', res.data.access_token, { expires: 7 })
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(error => {
+        console.log(error)
       })
-  };
+  }
 
   render() {
     return (
       <MuiThemeProvider>
-      <div>
-        <Helmet
-          title={this.props.data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header siteTitle={this.props.data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
+          <Helmet
+            title={this.props.data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          />
+          <Header siteTitle={this.props.data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0,
+            }}
+          >
+            {this.props.children()}
+          </div>
         </div>
-      </div>
       </MuiThemeProvider>
     )
   }
-};
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Cookies from 'js-cookie'
 
 const IndexPage = () => (
   <div>
